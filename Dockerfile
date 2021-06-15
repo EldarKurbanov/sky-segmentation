617d9a2006a796056ffbc8c47f1c48a7f1d62881
Ubuntu <ubuntu@ip-172-31-37-174>
2021-06-15 20:27:10 +0000

    Fix all things

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # syntax=docker/dockerfile:1
-FROM ubuntu
+FROM tensorflow/tensorflow:2.2.2-gpu-py3
 WORKDIR /code
 ENV FLASK_APP=app.py
 ENV FLASK_RUN_HOST=0.0.0.0
@@ -7,9 +7,13 @@ ENV TZ=Europe/Volgograd
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 RUN apt-get update
 RUN apt-get -y install python3 python3-pip ffmpeg libsm6 libxext6 git
+#RUN ln -s /usr/local/cuda/lib64/stubs/libcuda.so /usr/local/cuda/lib64/stubs/libcuda.so.1
+#RUN LD_LIBRARY_PATH=/usr/local/cuda/lib64/stubs/:$LD_LIBRARY_PATH python3 setup.py install 
+#RUN rm /usr/local/cuda/lib64/stubs/libcuda.so.1
 COPY requirements.txt requirements.txt
 RUN pip install -q git+https://github.com/tensorflow/examples.git
-RUN pip install -r requirements.txt
+RUN pip install --use-feature=2020-resolver -r requirements.txt
 EXPOSE 5000
 COPY . .
-CMD ["flask", "run"]
+CMD ["python3", "app.py"]
+#CMD ["flask", "run"]
--- TensorflowLite_UNet/config/skyseg_config.py
+++ TensorflowLite_UNet/config/skyseg_config.py
@@ -1,5 +1,5 @@
 # define the path to the images directory
-path = '/Users/siddhantbansal/Desktop/Python/Personal_Projects/Cats_vs_Dogs/dataset/kaggle_dogs_vs_cats/train'
+# path = '/Users/siddhantbansal/Desktop/Python/Personal_Projects/Cats_vs_Dogs/dataset/kaggle_dogs_vs_cats/train'
 # since we do not have access to validation data we need to take a number of images from train and test on them
 NUM_CLASSES = 2
 NUM_VAL_IMAGES = 1250 * NUM_CLASSES
@@ -9,23 +9,23 @@ NUM_TEST_IMAGES = 1250 * NUM_CLASSES
 #TRAIN_HDF5 = '/home/ubuntu/PycharmProjects/Sky_Segmentation/TensorflowLite_UNet/Dataset/hdf5/train3c.hdf5'
 
 #TRAIN_HDF5 = '/home/ubuntu/PycharmProjects/Sky_Segmentation/TensorflowLite_UNet/Dataset/hdf5/train_3_1c_256_norm.hdf5'
-TRAIN_HDF5 = '/home/ubuntu/PycharmProjects/Sky_Segmentation/TensorflowLite_UNet/Dataset/hdf5/train_3_1c_768_norm_full.hdf5'
+TRAIN_HDF5 = '/code/TensorflowLite_UNet/Dataset/hdf5/train_3_1c_768_norm_full.hdf5'
 
 #VAL_HDF5 = '/home/ubuntu/PycharmProjects/Sky_Segmentation/TensorflowLite_UNet/Dataset/hdf5/val_3_1c_256_norm.hdf5'
-VAL_HDF5 = '/home/ubuntu/PycharmProjects/Sky_Segmentation/TensorflowLite_UNet/Dataset/hdf5/val_3_1c_768_norm.hdf5'
+VAL_HDF5 = '/code/TensorflowLite_UNet/Dataset/hdf5/val_3_1c_768_norm.hdf5'
 
 #TEST_HDF5 = '/home/ubuntu/PycharmProjects/Sky_Segmentation/TensorflowLite_UNet/Dataset/hdf5/test_3_1c_256_norm.hdf5'
-TEST_HDF5 = '/home/ubuntu/PycharmProjects/Sky_Segmentation/TensorflowLite_UNet/Dataset/hdf5/test_3_1c_768_norm.hdf5'
+TEST_HDF5 = '/code/TensorflowLite_UNet/Dataset/hdf5/test_3_1c_768_norm.hdf5'
 
 # path to the output model file
-MODEL_PATH = '/home/ubuntu/PycharmProjects/Sky_Segmentation/TensorflowLite_UNet/model'
+MODEL_PATH = '/code/TensorflowLite_UNet/model'
 
-MODEL_ARCHITECT_DIR = '/home/ubuntu/PycharmProjects/Sky_Segmentation/TensorflowLite_UNet/Visualize_architect'
+MODEL_ARCHITECT_DIR = '/code/TensorflowLite_UNet/Visualize_architect'
 
-BEST_WEIGHTS_STORE_DIR = '/home/ubuntu/PycharmProjects/Sky_Segmentation/TensorflowLite_UNet/Best_weights'
+BEST_WEIGHTS_STORE_DIR = '/code/TensorflowLite_UNet/Best_weights'
 
 # define the path to the dataset mean
-DATASET_MEAN = '/home/ubuntu/PycharmProjects/Sky_Segmentation/TensorflowLite_UNet/Output/mean_values.json'
+DATASET_MEAN = '/code/TensorflowLite_UNet/Output/mean_values.json'
 
 # define the path to the output directory used for storing plots, classification_reports etc.
-OUTPUT_PATH = '/home/ubuntu/PycharmProjects/Sky_Segmentation/TensorflowLite_UNet/Output'
+OUTPUT_PATH = '/code/TensorflowLite_UNet/Output'
